2fe999651c3fb63c2f5f27c4165fba5735ee519a
muuuby <52901400+muuuby@users.noreply.github.com>
2021-03-31 17:58:47 +0800

    Add files via upload
    version 2.0

--- src/start.py
+++ src/start.py
@@ -11,21 +11,19 @@ def learn_embeddings(walks):
 
 	print "Training..."
 	model = Word2Vec(walks, size= 128, window= 10, min_count= 0, sg=1, workers = 8, iter= 1)
-	model.wv.save_word2vec_format("ontology2vec/test/test_embedding.vector")
+	model.wv.save_word2vec_format("thesis/test/test_embedding.vector")
 	
 	return
 
+root = wn.synsets('entity')[0]
+node_depth = {}
+nx_G = ontology2graph.o2graph(root, node_depth) 
 
-root = wn.synsets('cat')[0]
-nx_G = ontology2graph.o2graph(root) 
-nx.write_edgelist(nx_G ,'ontology2vec/test/test_edge.edgelist',data=['weight'])
-
-p = 1 #scale
 num_walks = 10
-walk_length = 80
+walk_length = 40
+p = 5 #scale
 G = ontology2vec.Graph(nx_G)
 G.transition_probs(p)
 walks = G.simulate_walks(num_walks, walk_length)
 learn_embeddings(walks)
-
 
